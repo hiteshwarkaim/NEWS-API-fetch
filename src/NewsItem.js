c734e6e9abaa923f9b352d9a58eff7d60c9ba3fa
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-04-26 12:07:23 +0530

    fetch new-api and render successfuly 1

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -1319,7 +1319,7 @@ export default class NewsItem extends Component {
     let {title, description,imageUrl,newsUrl}=this.props;
     return (                    
             <div className="card" style={{width: "18rem"}} >
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={imageUrl? "default.jpg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
