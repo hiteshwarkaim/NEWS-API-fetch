33cb3c6c37b408ec26498225b417239c9b36835d
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-04-26 13:59:46 +0530

    fetch new-api and render successfuly 2

--- src/News.js
+++ src/News.js
@@ -1,48 +1,50 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
+  articles = [];
 
-  articles = [
-    
-    
-  ];
-
-constructor(){
+  constructor() {
     super();
     console.log("constructor");
-    this.state={
-        articles:this.articles,
-        loading:false
+    this.state = {
+      articles: this.articles,
+      loading: false,
+    };
+  }
 
-    }
-}
-
-//it is run after render
-async componentDidMount(){
-  let url="https://newsapi.org/v2/everything?q=Apple&sortBy=popularity&apiKey=71b90f466e9746ac821cd8d22f56f6d1";
-  let data = await fetch(url);
-  let parsedData=await data.json();
-  this.setState({articles:parsedData.articles})
-  console.log(data);
-}
-
+  //it is run after render
+  async componentDidMount() {
+    //a synch function can wait inside its body till some promise become resolve
+    let url =
+      "https://newsapi.org/v2/everything?q=Apple&sortBy=popularity&apiKey=71b90f466e9746ac821cd8d22f56f6d1";
+    let data = await fetch(url); //it returns promise
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles });
+    // console.log(parsedData);
+  }
 
   render() {
     return (
       <>
-            <div className="container">
-              <h1 className='text-primary'>Top Headlines</h1>
-              <div className="row">
-              {this.state.articles.map((article)=>{
-                return <div className="col" key={article.url}>
-                        <NewsItem  title={article.title.slice(0,45)} description={article.description.slice(0,88)} imageUrl={article.urlToImage} newsUrl={article.url}/>
-                    </div>
-              })}
-                  
-              </div>
-    </div> 
+        <div className="container">
+          <h1 className="text-primary">Top Headlines</h1>
+          <div className="row">
+            {this.state.articles.map((article) => {
+              return (
+                <div className="col gy-5" key={article.url}>
+                  <NewsItem
+                    title={article.title.slice(0, 45)}
+                    description={article.description.slice(0, 88)}
+                    imageUrl={article.urlToImage}
+                    newsUrl={article.url}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </>
-    )
+    );
   }
 }
